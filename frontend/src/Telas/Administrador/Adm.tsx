0e5b5a8cae3ce303bb6bdf8fec5ef904ad8c7eda
Luis Fylipe <luisfylipe1802@gmail.com>
2026-06-18 16:00:26 -0300

    feat(pagina-adm): Logica para funcionamento de dados no detalhes do adm na pagina de administrador.

--- frontend/src/Telas/Administrador/Adm.tsx
+++ frontend/src/Telas/Administrador/Adm.tsx
@@ -1,13 +1,18 @@
 import HeaderPaginaAdministrador from "./HeaderPaginaAdministrador/Header";
 import { ResumoSectionAdm } from "./ResumoDetalhesAdm/ResumoDetalhesAdm";
 import TabelaClientes from "./TabelaClientePaginaAdministrador/TabelaClientes";
+import { useClientes } from "../../hooks/useClientes";
+import { useFaturas } from "../../hooks/useFaturas";
 
 export default function PaginaAdministrador() {
+  const { clientes } = useClientes();
+  const { faturas } = useFaturas(clientes);
+
   return (
     <>
       <HeaderPaginaAdministrador />
-      <ResumoSectionAdm faturas={[]} />
-      <TabelaClientes/>
+      <ResumoSectionAdm faturas={faturas} />
+      <TabelaClientes />
     </>
   );
 }
