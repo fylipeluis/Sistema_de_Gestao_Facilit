c56947d75b3fc1504233e07830e1f9a8085cebac
Luis Fylipe <luisfylipe1802@gmail.com>
2026-07-08 09:26:18 -0300

    feat(reformulacao-adm): Novas mudanças de cores de componentes.

--- frontend/src/Telas/Administrador/Adm.tsx
+++ frontend/src/Telas/Administrador/Adm.tsx
@@ -4,29 +4,38 @@ import { ResumoSectionAdm } from "./ResumoDetalhesAdm/ResumoDetalhesAdm";
 import type { Secao } from "./SidebarPaginaAdministrador/SidebarPaginaAdm";
 import TabelaClientes from "./TabelaClientePaginaAdministrador/TabelaClientes";
 import Sidebar from "./SidebarPaginaAdministrador/SidebarPaginaAdm"
+import "./adm.css";
 
 export default function PaginaAdministrador() {
   const [secaoAtiva, setSecaoAtiva] = useState<Secao>("dashboard");
 
   return (
-    <>
-      <HeaderPaginaAdministrador />
-      <Sidebar secaoAtiva={secaoAtiva} onSelecionar={setSecaoAtiva} />
 
-      <main className="app-main">
-        {secaoAtiva === "dashboard" && (
-          <>
-            <ResumoSectionAdm />
-            <TabelaClientes />
-          </>
-        )}
+    <div className="layout">
 
-        {secaoAtiva === "clientes" && <p>Seção Clientes — a implementar</p>}
-        {secaoAtiva === "contratos" && <p>Seção Contratos — a implementar</p>}
-        {secaoAtiva === "cobrancas" && <p>Seção Cobranças — a implementar</p>}
-        {secaoAtiva === "pagamentos" && <p>Seção Pagamentos — a implementar</p>}
-        {secaoAtiva === "relatorios" && <p>Seção Relatórios — a implementar</p>}
-      </main>
-    </>
+      <Sidebar
+        secaoAtiva={secaoAtiva}
+        onSelecionar={setSecaoAtiva}
+      />
+
+      <div className="content">
+        <HeaderPaginaAdministrador />
+
+        <main className="app-main">
+          {secaoAtiva === "dashboard" && (
+            <>
+              <ResumoSectionAdm />
+              <TabelaClientes />
+            </>
+          )}
+
+          {secaoAtiva === "clientes" && <p>Clientes</p>}
+          {secaoAtiva === "contratos" && <p>Contratos</p>}
+          {secaoAtiva === "cobrancas" && <p>Cobranças</p>}
+          {secaoAtiva === "pagamentos" && <p>Pagamentos</p>}
+          {secaoAtiva === "relatorios" && <p>Relatórios</p>}
+        </main>
+      </div>
+    </div>
   );
 }
--- frontend/src/Telas/Administrador/HeaderPaginaAdministrador/HeaderComponents/TituloHeaderPaginaAdm.tsx
+++ frontend/src/Telas/Administrador/HeaderPaginaAdministrador/HeaderComponents/TituloHeaderPaginaAdm.tsx
@@ -3,7 +3,7 @@ import "./TituloHeaderPaginaAdm.css";
 export function TituloHeaderPaginaAdm() {
   return (
     <div className="title-header">
-      <h1>Painel Administrativo</h1>
+      <h1>Painel <span>Administrativo</span></h1>
     </div>
   );
 }
